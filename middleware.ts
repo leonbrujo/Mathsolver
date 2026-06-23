dab1a313c80e23ac5998a7e5896994c9c760dbab
leonbrujo <92657260+leonbrujo@users.noreply.github.com>
2026-06-23 17:11:31 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    const { userId } = await auth()
-    if (!userId) {
-      return NextResponse.redirect(new URL('/sign-in', request.url))
-    }
+    await (await auth()).redirectToSignIn()
   }
 })
 
